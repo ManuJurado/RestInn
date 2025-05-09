d1e0ed290d973002dee31f193d67b2b3193c21d9
Lisandstone <guidodevoto@gmail.com>
2025-05-09 19:33:20 -0300

    Integración de atributos de Empleado.

--- src/main/java/RestInn/entity/Empleado.java
+++ src/main/java/RestInn/entity/Empleado.java
@@ -1,4 +1,21 @@
 package RestInn.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Empleado {
+    @Id
+    private Long id;
+    private String nombre, apellido, dni;
+    private Double sueldoHora;
+    private Integer horasExtra;
 }
