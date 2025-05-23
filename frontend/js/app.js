dc1b6c82b031864ade0e4909103df77c17dc85be
ManuJurado <manu27sosa@gmail.com>
2025-05-23 18:44:33 -0300

    Agregada logica basica de front(dejar para el final)

---
+++ frontend/js/app.js
@@ -0,0 +1,12 @@
+// frontend/js/app.js
+fetch('http://localhost:8080/api/habitaciones')
+  .then(response => response.json())
+  .then(data => {
+    const lista = document.getElementById('habitaciones-list');
+    data.forEach(habitacion => {
+      const li = document.createElement('li');
+      li.textContent = `Habitación ${habitacion.numero} - Tipo: ${habitacion.tipo}`;
+      lista.appendChild(li);
+    });
+  })
+  .catch(error => console.error('Error al obtener habitaciones:', error));
