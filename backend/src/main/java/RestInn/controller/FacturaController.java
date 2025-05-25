2308979de32929230530d2b6fcd4104d75d513b7
Emiliano Salias <emiliano.salias@outlook.com>
2025-05-25 20:30:41 -0300

    - Se agregan las anotaciones de Spring DATA JPA en las entidades de facturacion

--- backend/src/main/java/RestInn/controller/FacturaController.java
+++ backend/src/main/java/RestInn/controller/FacturaController.java
@@ -1,7 +1,11 @@
 package RestInn.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FacturaController {
+@RequestMapping("/facturas")
+public class  FacturaController {
+
 }
--- backend/src/main/java/RestInn/entities/cobranzas/Factura.java
+++ backend/src/main/java/RestInn/entities/cobranzas/Factura.java
@@ -3,7 +3,9 @@ package RestInn.entities.cobranzas;
 import RestInn.entities.Reserva;
 import RestInn.entities.enums.EstadoFactura;
 import RestInn.entities.enums.MetodoPago;
+import RestInn.entities.usuarios.Cliente;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,22 +13,60 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Entity
-@Getter
-@Setter
+@Table(name = "facturas")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "factura_id")
     private Long id;
-    private LocalDate fechaEmision;
-    private List<Consumo> consumos;
-    private BigDecimal subtotal;  // reserva + consumos
-    private MetodoPago metodoPago;
-    private Double cuotas;
-    private Double descuento;   // porcentual
-    private Double interes;     // porcentual
-    private BigDecimal totalFinal;
-    private EstadoFactura estado;
-    private Double haber;   // pagado
-    private Double debe;    // deuda
 
+    @OneToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @OneToOne
+    @JoinColumn(name = "reserva_id")
+    private Reserva reserva;
+
+    @Column(name = "fecha_emision", nullable = false)
+    private LocalDate fechaEmision;
+
+    @OneToMany(mappedBy = "factura", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Consumo> consumos;
+
+    @Column(nullable = false)
+    @DecimalMin("0.00")
+    private BigDecimal subtotal;  // reserva + consumos
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MetodoPago metodoPago;
+
+    @Column(nullable = false)
+    private Integer cuotas;
+
+    @DecimalMin("0.00")
+    private BigDecimal descuento;   // porcentual
+
+    @DecimalMin("0.00")
+    private BigDecimal interes;     // porcentual
+
+    @Column(name = "total_final", nullable = false)
+    @DecimalMin("0.00")
+    private BigDecimal totalFinal;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EstadoFactura estado;
+
+    @DecimalMin("0.00")
+    private BigDecimal haber;   // pagado
+
+    @DecimalMin("0.00")
+    private BigDecimal debe;    // deuda
 }
+
