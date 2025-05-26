5f012ecf1c5f4af5d659eec5db57e551e1155edc
Emiliano Salias <emiliano.salias@outlook.com>
2025-05-25 21:18:34 -0300

    - Se hacen modificaciones de configuracion

--- backend/src/main/java/RestInn/controller/apiController/FacturaController.java
+++ backend/src/main/java/RestInn/controller/apiController/FacturaController.java
@@ -1,6 +1,5 @@
-package RestInn.controller;
+package RestInn.controller.apiController;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
