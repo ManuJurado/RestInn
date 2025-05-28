347b36ffc4c69af3f0cc04c51d2710f34da74c2e
ManuJurado <manu27sosa@gmail.com>
2025-05-28 01:21:29 -0300

    Correccion de logica de huespedes como entidad. Se deja como embedded de reservas. Implementacion de metodo de creacion de reservas como cliente logueado. Cambio de almacenamiento de token de localhost a session(sigue siendo stateless).

--- backend/src/main/java/RestInn/entities/Huesped.java
+++ backend/src/main/java/RestInn/entities/Huesped.java
@@ -3,20 +3,15 @@ package RestInn.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
+@Embeddable
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Huesped {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private Boolean activo; //Borrado lógico.
     private String nombre;
     private String apellido;
     private String dni;
-    private Long telefono;
 }
+
